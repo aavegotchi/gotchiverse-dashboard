93173fa6959059a851c2f36c45e22555d53a7085
Frank <frank.pfeift@protonmail.com>
2022-06-08 17:16:18 +0200

    added first queries

--- fetcher.js
+++ fetcher.js
@@ -21,5 +21,99 @@ const gltrStakingSubgraph = apollo.createApolloFetch({
 });
 
 export default async (url) => {
-  return {};
+  const getAlchemicaTotalSupply = async () => {
+    let query = `{erc20Contracts {
+      name,
+      totalSupply {
+        id
+        value
+      }
+    }}`;
+    const result = await alchemicaSubgraph({ query });
+
+    return result.data.erc20Contracts.map((e) => ({
+      name: e.name,
+      totalSupply: e.totalSupply.value,
+    }));
+  };
+
+  const getStats = async () => {
+    let query = `{stat(id:"overall") {
+      countChannelAlchemicaEvents
+      countParcelInstallations
+      countInstallationTypes
+      countUpgradesInitiated
+      alchemicaSpendOnInstallations
+      alchemicaSpendOnUpgrades
+      alchemicaSpendOnTiles
+      alchemicaSpendTotal
+      alchemicaChanneledTotal
+      alchemicaClaimedTotal
+      alchemicaExitedTotal
+      tilesMinted
+      installationsMintedTotal
+      installationsUpgradedTotal
+    }}`;
+
+    const result = await gotchiverseSubgraph({ query });
+
+    return result.data.stat;
+  };
+
+  const getGotchis = async () => {
+    let query = `{statistics {
+      aavegotchisClaimed 
+    }}`;
+
+    let result = await coreMaticSubgraph({ query });
+    let gotchisClaimed = result.data.statistics[0].aavegotchisClaimed;
+
+    query = `{user(id:"0x0000000000000000000000000000000000000000") {
+      gotchisOwned(first: 1000) {
+        id
+      }
+    }}`;
+
+    result = await coreMaticSubgraph({ query });
+    let gotchisSacrificed = result.data.user.gotchisOwned.length;
+
+    return { gotchisClaimed, gotchisSacrificed };
+  };
+
+  const stats = await getStats();
+  const totalSupply = await getAlchemicaTotalSupply();
+  const gotchis = await getGotchis();
+
+  return {
+    alchemica: {
+      minted: stats.alchemicaChanneledTotal.map((e, i) => ({
+        name: totalSupply[i].name,
+        value: e,
+      })),
+      totalSupply: totalSupply,
+      spendOn: {
+        tiles: stats.alchemicaSpendOnTiles,
+        installations: stats.alchemicaSpendOnInstallations,
+        upgrades: stats.alchemicaSpendOnUpgrades,
+      },
+    },
+    tilesMinted: stats.tilesMinted,
+    installationsMinted: stats.installationsMintedTotal,
+    installationsUpgraded: stats.installationsUpgradedTotal,
+    numberOfChannels: stats.countChannelAlchemicaEvents,
+    gltrSpendOnUpgrades: "todo",
+    activeWallets: "todo",
+    playersBanned: "todo",
+    playersUnbanned: "todo",
+    alchemicaSoldByBannedPlayers: "todo",
+    gltrStaking: {
+      stakers: "todo",
+      stakedPoolTokens: ["todo", "todo", "todo", "todo", "todo", "todo"],
+    },
+    numberOfExtractors: "todo",
+    gotchisSummoned: gotchis.gotchisClaimed - gotchis.gotchisSacrificed,
+    gotchisSacrificed: gotchis.gotchisSacrificed,
+    gotchisBorrowed: "todo",
+    numberOfGotchisChanneled: "todo",
+  };
 };
